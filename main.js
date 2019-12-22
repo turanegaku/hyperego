492b14a6220c8de1f49a76a7dcb8614194fba9d9
Glitch (pumped-chopper) <none>
2019-12-22 05:29:39 +0000

    🌃🎃 Checkpoint ./package.json:9632548/588 ./main.js:9632548/5294 ./buki.csv:9632548/93

--- main.js
+++ main.js
@@ -1,46 +1,57 @@
 // Response for Uptime Robot
-const http = require('http');
-http.createServer(function(request, response) {
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.end('Discord bot is active now \n');
-}).listen(8080);
+const http = require("http");
+http
+  .createServer(function(request, response) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Discord bot is active now \n");
+  })
+  .listen(8080);
 
 // Discord bot implements
-const discord = require('discord.js');
+const discord = require("discord.js");
 const client = new discord.Client();
 
-client.on('ready', message => {
-	console.log('bot is ready!');
+const csv = require("csv");
+const fs = require("fs");
+
+let a = fs.createReadStream("buki.csv").pipe(csv.parse({ columns: true }));
+console.log(a)
+
+client.on("ready", message => {
+  console.log("bot is ready!");
 });
 
 //prefixの設定
-const prefix = '!';
+const prefix = "!";
 
-client.on('message', message => {
+client.on("message", message => {
   //botに反応しなくなる奴
-  if(message.author.bot) return;
+  if (message.author.bot) return;
   //メンションが来たら｢呼びましたか？｣と返す
-	if(message.isMemberMentioned(client.user)) {
-		message.reply('呼びましたか？');
-	}
+  if (message.isMemberMentioned(client.user)) {
+    message.reply("呼びましたか？");
+  }
   //argumentなどの処理
-  if(message.content.indexOf(prefix) !== 0) return;
-  
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  if (message.content.indexOf(prefix) !== 0) return;
+
+  const args = message.content
+    .slice(prefix.length)
+    .trim()
+    .split(/ +/g);
   const command = args.shift().toLowerCase();
-  
+
   //!pingと打ったらpong!が帰ってくる
-  if(command === "random") {
+  if (command === "random") {
     message.member.voiceChannel.members.forEach(member => {
-      let user = member.user
-      console.log(user)
-    })
+      let user = member.user;
+      console.log(user);
+    });
   }
 });
 
-if(process.env.DISCORD_BOT_TOKEN == undefined) {
-	console.log('please set ENV: DISCORD_BOT_TOKEN');
-	process.exit(0);
+if (process.env.DISCORD_BOT_TOKEN == undefined) {
+  console.log("please set ENV: DISCORD_BOT_TOKEN");
+  process.exit(0);
 }
 
-client.login( process.env.DISCORD_BOT_TOKEN );
+client.login(process.env.DISCORD_BOT_TOKEN);
